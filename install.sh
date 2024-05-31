c9f1631cb77d9bac95a170d69051251a388a82b5
Lewis Jones <ljones140@gmail.com>
2024-05-31 18:26:05 +0100

    Try to symlink the nvim config

--- install.sh
+++ install.sh
@@ -9,18 +9,6 @@ ln -s $(pwd)/vimrc $HOME/.vimrc
 ln -s $(pwd)/vim $HOME/.vim
 ln -s $(pwd)/zshrc $HOME/.zshrc
 
-ln -s $(pwd)/config/nvim/init.vim $HOME/.config/nvim/init.vim
-
-ln -s $(pwd)/config/nvim/lua/init.lua $HOME/.config/nvim/lua/init.lua
-ln -s $(pwd)/config/nvim/lua/lewis/init.lua $HOME/.config/nvim/lua/lewis/init.lua
-ln -s $(pwd)/config/nvim/lua/lewis/packer.lua $HOME/.config/nvim/lua/lewis/packer.lua
-ln -s $(pwd)/config/nvim/lua/lewis/remap.lua $HOME/.config/nvim/lua/lewis/remap.lua
-
-ln -s $(pwd)/config/nvim/after/plugin/harpoon.lua $HOME/.config/nvim/after/plugin/harpoon.lua
-ln -s $(pwd)/config/nvim/after/plugin/lsp.lua $HOME/.config/nvim/after/plugin/lsp.lua
-ln -s $(pwd)/config/nvim/after/plugin/neotest.lua $HOME/.config/nvim/after/plugin/neotest.lua
-ln -s $(pwd)/config/nvim/after/plugin/telescope.lua $HOME/.config/nvim/after/plugin/telescope.lua
-ln -s $(pwd)/config/nvim/after/plugin/treesitter.lua $HOME/.config/nvim/after/plugin/treesitter.lua
-ln -s $(pwd)/config/nvim/after/plugin/undotree.lua $HOME/.config/nvim/after/plugin/undotree.lua
+mkdir -p $HOME/.config/nvim/ && ln -s $(pwd)/config/nvim/* $HOME/.config/nvim
 
 vim -Es -u $HOME/.vimrc -c "PlugInstall | qa"
