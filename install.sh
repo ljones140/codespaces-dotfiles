138a5ddd688667644818777fbe06e8a6ff165278
Lewis Jones <ljones140@gmail.com>
2024-06-13 11:19:04 +0100

    Remove packer install

--- install.sh
+++ install.sh
@@ -12,6 +12,3 @@ ln -s $(pwd)/zshrc $HOME/.zshrc
 mkdir -p $HOME/.config/nvim/ && ln -s $(pwd)/config/nvim/* $HOME/.config/nvim
 
 vim -Es -u $HOME/.vimrc -c "PlugInstall | qa"
-
-cd $HOME/.config/nvim/lua
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync
