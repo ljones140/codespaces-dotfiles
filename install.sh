e20d3c4ff4e785450cf94e9f0c7185df13fe0af5
Lewis Jones <ljones140@gmail.com>
2024-06-13 11:31:57 +0100

    Try copying over zsrc and merging

--- install.sh
+++ install.sh
@@ -7,8 +7,24 @@ set -x
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
 ln -s $(pwd)/vimrc $HOME/.vimrc
 ln -s $(pwd)/vim $HOME/.vim
-ln -s $(pwd)/zshrc $HOME/.zshrc
 
 mkdir -p $HOME/.config/nvim/ && ln -s $(pwd)/config/nvim/* $HOME/.config/nvim
 
 vim -Es -u $HOME/.vimrc -c "PlugInstall | qa"
+
+ln -s $(pwd)/zshrc $HOME/.zshrc
+# Save the existing zshrc file
+if [[ -n "$CODESPACES" && -e "$HOME/.zshrc" ]]; then
+    cp "$HOME/.zshrc" "$HOME/.zshrc_cs"
+
+    # Clean up unwanted lines from the Codespaces zshrc file
+    sed -i '1,/# Example aliases/d' "$HOME/.zshrc_cs"
+fi
+
+# Copy over .zshrc file
+cp $(pwd)/zshrc $HOME/.zshrc
+
+# Merge the zshrc with the pre-existing one
+if [[ -n "$CODESPACES" && -e "$HOME/.zshrc_cs" ]]; then
+    cat "$HOME/.zshrc_cs" >> "$HOME/.zshrc"
+fi
